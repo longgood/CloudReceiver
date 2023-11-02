e3e17730de1631f2c936762ca8879180f1f99308
longgood <ray@longgood.com.tw>
2023-11-02 10:20:43 +0800

    不同系統下載的路徑

--- apps/config.py
+++ apps/config.py
@@ -29,7 +29,7 @@ class ProductionConfig(Config):
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
         config('DB_ENGINE', default='postgresql'),
-        config('DB_USERNAME', default='appseed'),
+        config('DB_USERNAME', default='abccd'),
         config('DB_PASS', default='pass'),
         config('DB_HOST', default='localhost'),
         config('DB_PORT', default=5432),
--- apps/report/routes.py
+++ apps/report/routes.py
@@ -16,9 +16,17 @@ def index():
     for data in data_list:
         print("資料:",data.score, ",",data.raw_file)
     return render_template('report/index.html', segment='index',data_list=data_list)
-    
+
+
+import sys
+if sys.platform.startswith('win32'):
+    #file_folder="D:\\projects\\CloudReceiver\\download_files"
+    file_folder="..\\download_files"
+
+elif sys.platform.startswith('linux'):
+    file_folder="/home/ray/CloudReceiver/download_files"
 @blueprint.route('/download/<filename>')
 def download_file(filename):
-    file_folder="D:\\projects\\CloudReceiver\\download_files"
+    global file_folder
     return send_from_directory(file_folder, filename, as_attachment=True)
 
